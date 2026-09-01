4c6f339c9e1abc03ce776b7ad05af6d61244a874
Eduardo <dunumata07@gmail.com>
2026-09-01 15:06:53 -0300

    sidebar

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="NMT | Portal do Aluno",
     page_icon="⚡",
     layout="centered",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="auto"
 )
 
 # Load Custom CSS
